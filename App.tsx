884d1f2b18b1f6cdebe4e2696dc3991010b752bd
miulerbm <miulerbm00@gmail.com>
2024-01-06 17:40:27 -0500

    Primera pantalla inicial

--- App.tsx
+++ App.tsx
@@ -1,23 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HOLA MIULER</Text>
-      <StatusBar style="auto" />
+      {/* EL fondo: */}
+      <Image
+        source={require("./assets/chef.jpg")}
+        style={styles.imageBackground}
+      />
+
+      {/* El formulario: */}
+      <View style={styles.form}></View>
     </View>
   );
 }
-
 
 // HOT RELOAD
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  // Creamos un estilo para la imagen de fondo:
+  imageBackground: {
+    // Se hace que la imagen ocupe todo el ancho y alto de la pantalla:
+    width: "100%",
+    height: "100%",
+    bottom: "30%", // Con esto le decimos que suba un poco.
+    // Opacidad de la imagen
+    opacity: 0.7,
+  },
+  form: {
+    width: "100%",
+    height: "40%",
+    backgroundColor: "white",
+    position: "absolute",
+    bottom: 0,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
   },
 });
