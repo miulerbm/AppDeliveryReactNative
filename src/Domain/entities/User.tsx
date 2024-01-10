b9f6086fd99ebb3cd0d6d52a260387013fac3cb8
miulerbm <miulerbm00@gmail.com>
2024-01-09 19:14:08 -0500

    Setting up model view view model pattern

---
+++ src/Domain/entities/User.tsx
@@ -0,0 +1,12 @@
+// Vamos a crear la clase usuario.
+// Herramienta: app quicktype io
+
+export interface User {
+  id?: string;
+  name: string;
+  lastname: string;
+  phone: string;
+  email: string;
+  password: string;
+  confirmpassword: string;
+}
--- src/Presentation/views/register/ViewModel.tsx
+++ src/Presentation/views/register/ViewModel.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ApiDelivery } from "../../../Data/sources/remote/api/ApiDelivery";
+import { RegisterAuthUseCase } from "../../../Domain/useCases/auth/RegisterAuth";
 
 const RegisterViewModel = () => {
   const [values, setValues] = useState({
@@ -9,30 +10,18 @@ const RegisterViewModel = () => {
     email: "",
     password: "",
     confirmpassword: "",
-    image: "",
   });
 
-  // con OnChange, definimos que setValues modifique a uno de los atributos del objeto
-  // definido, le decimos, esta "propiedad" <- "valor", le cargamos el valor a un estado.
   const onChange = (property: string, value: any) => {
     setValues({ ...values, [property]: value });
   };
 
-  // Evento para cuando se pulse el botón CONFIRMAR (componente externo)
-
   const register = async () => {
-    // En el botón Register, vamos a comunicarnos con la base de datos
-    // mediante una petición HTTP para hacer un POST y añadir un usuario nuevo.
-    try {
-      // El post de axios tiene la ruta y los valores como atributos.
-      const response = await ApiDelivery.post("/users/create", values); //Esta es la ruta definida en el backend para crear un user
-      console.log("response: ", JSON.stringify(response.data)); //Solo quiero ver la parte de data, en response.
-    } catch (error) {
-      console.log("error: ", error);
-    }
+    const { result, error } = await RegisterAuthUseCase(values);
+    console.log("RESULT", JSON.stringify(result));
+    console.log("ERROR", error);
   };
 
-  // Esta función hará accesible los estados y el método onChange
   return {
     ...values,
     onChange,
