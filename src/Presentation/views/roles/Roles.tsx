841cb7f921477bfcb64017913699bc9c3d9cd1a6
miulerbm <miulerbm00@gmail.com>
2024-01-12 16:26:49 -0500

    Carousel for roles added

--- src/Presentation/views/roles/Roles.tsx
+++ src/Presentation/views/roles/Roles.tsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dimensions, FlatList, Text, View } from "react-native";
 import useViewModel from "./ViewModel";
 import { RolesItem } from "./Item";
+import Carousel from "react-native-reanimated-carousel";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export const RolesScreen = () => {
   const { user } = useViewModel();
 
   //Obteniendo el total de la pantalla:
   const width = Dimensions.get("window").width;
+  const height = Dimensions.get("window").height;
+
+  // Arreglando la visualización del carrusel:
+  const [mode, setMode] = useState<any>("horizontal-stack");
+  const [snapDirection, setSnapDirection] = useState<"left" | "right">("left");
 
   return (
-    <View>
-      <FlatList
+    <GestureHandlerRootView
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <View>
+        {/* <FlatList
         data={user?.roles}
         renderItem={({ item }) => (
           <RolesItem rol={item} height={420} width={width - 100} />
         )}
         keyExtractor={(item) => item.id}
-      />
-    </View>
+      /> */}
+
+        <Carousel
+          loop={false}
+          width={width}
+          height={height / 1.5} // Ajustando la altura del elemento
+          autoPlay={false}
+          data={user?.roles!}
+          scrollAnimationDuration={5000}
+          // onSnapToItem={(index) => console.log("current index:", index)}
+          renderItem={({ item }) => (
+            <RolesItem rol={item} height={420} width={width - 100} />
+          )}
+          modeConfig={{
+            snapDirection,
+            stackInterval: 30,
+          }}
+          mode={mode}
+        />
+      </View>
+    </GestureHandlerRootView>
   );
 };
