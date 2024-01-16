7ef645931da449eda708afdf3842a1f35c51880c
miulerbm <miulerbm00@gmail.com>
2024-01-16 15:24:01 -0500

    Solving issues with petitions

--- src/Data/sources/remote/api/ApiDelivery.tsx
+++ src/Data/sources/remote/api/ApiDelivery.tsx
@@ -1,5 +1,7 @@
 // Vamos a utilizar axios para hacer las peticiones HTTP al backend
 import axios from "axios";
+import { LocalStorage } from "../../local/LocalStorage";
+import { User } from "../../../../Domain/entities/User";
 
 const ApiDelivery = axios.create({
   baseURL: "http://192.168.1.7:3000/api",
@@ -15,6 +17,26 @@ const ApiDeliveryForImage = axios.create({
     "Content-type": "multipart/form-data",
     accept: "application/json",
   },
+});
+
+//  INTERCEPTORS
+// Funcionan como middlewares, una capa intermedia entre la petición y el backend
+ApiDelivery.interceptors.request.use(async (config) => {
+  const data = await LocalStorage().getItem("user");
+  if (data) {
+    const user: User = JSON.parse(data as any);
+    config.headers!["Authorization"] = user.session_token!;
+  }
+  return config;
+});
+
+ApiDeliveryForImage.interceptors.request.use(async (config) => {
+  const data = await LocalStorage().getItem("user");
+  if (data) {
+    const user: User = JSON.parse(data as any);
+    config.headers!["Authorization"] = user.session_token!;
+  }
+  return config;
 });
 
 export { ApiDelivery, ApiDeliveryForImage };
