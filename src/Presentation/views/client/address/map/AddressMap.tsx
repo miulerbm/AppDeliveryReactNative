d5e84807ac07b72c9c9b389a538d982d65d45fdc
miulerbm <miulerbm00@gmail.com>
2024-02-06 09:31:04 -0500

    Adding components inside the map

--- src/Presentation/views/client/address/map/AddressMap.tsx
+++ src/Presentation/views/client/address/map/AddressMap.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
-import { Text, ToastAndroid, View } from "react-native";
+import { Image, Text, ToastAndroid, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import styles from "./Styles";
 import useViewModel from "./ViewModel";
+import { RoundedButton } from "../../../../components/RoundedButton";
+import stylesMap from "./StylesMap";
 
 export const ClientAddressMapScreen = () => {
   const { messagePermissions, position, mapRef } = useViewModel();
@@ -17,9 +19,23 @@ export const ClientAddressMapScreen = () => {
     <View style={styles.container}>
       <MapView
         ref={mapRef}
+        customMapStyle={stylesMap}
         style={{ height: "100%", width: "100%" }}
         provider={PROVIDER_GOOGLE}
       />
+
+      <Image
+        style={styles.imageLocation}
+        source={require("../../../../../../assets/location_home.png")}
+      />
+
+      <View style={styles.refPoint}>
+        <Text style={styles.refPointText}>Punto de referencia</Text>
+      </View>
+
+      <View style={styles.buttonRefPoint}>
+        <RoundedButton text="SELECCIONAR PUNTO" onPress={() => {}} />
+      </View>
     </View>
   );
 };
--- src/Presentation/views/client/address/map/Styles.tsx
+++ src/Presentation/views/client/address/map/Styles.tsx
@@ -4,6 +4,32 @@ const ClientAddressMapStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageLocation: {
+    height: 65,
+    width: 65,
+    justifyContent: "center",
+    position: "absolute",
+  },
+  refPoint: {
+    position: "absolute",
+    backgroundColor: "#d4d4d4",
+    width: "70%",
+    paddingVertical: 4,
+    top: 40,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  refPointText: {
+    textAlign: "center",
+  },
+  buttonRefPoint: {
+    position: "absolute",
+    bottom: 40,
+    width: "70%",
   },
 });
 
